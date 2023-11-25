ea65b9b51ca5367bf7221b357681a79b109e96c3
nischithbo <nischithbo23@gmail.com>
2023-11-25 11:52:31 -0500

    COnstants_file

---
+++ src/Constants.java
@@ -0,0 +1,31 @@
+package src;
+
+public class Constants {
+        public enum MessageType {
+            CHOKE('0'), UNCHOKE('1'), INTERESTED('2'), NOT_INTERESTED('3'),
+            HAVE('4'), BITFIELD('5'), REQUEST('6'), PIECE('7');
+
+            private final char code;
+
+            MessageType(char code) {
+                this.code = code;
+            }
+
+            public char getCode() {
+                return code;
+            }
+
+            public static MessageType fromCode(char code) {
+                for (MessageType messageType : MESSAGE_TYPES) {
+                    if (messageType.getCode() == code) {
+                        return messageType;
+                    }
+                }
+                throw new IllegalArgumentException("No MessageType with code: " + code);
+            }
+
+            private static final MessageType[] MESSAGE_TYPES = MessageType.values();
+        }
+
+        // Add other constants or enums as needed
+}
